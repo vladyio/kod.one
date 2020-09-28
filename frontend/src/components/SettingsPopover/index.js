f0eeef693e516db3582891ee51c0a3a7eee8489d
Uladzislau Andreyeu <me@vld.by>
2020-09-28 20:43:51 +0300

    Add more dummy inputs for settings

--- frontend/src/components/SettingsPopover/index.js
+++ frontend/src/components/SettingsPopover/index.js
@@ -13,18 +13,25 @@ import {
   PopoverCloseButton,
   PopoverFooter,
   FormLabel,
+  Editable,
+  EditablePreview,
+  EditableInput,
   Input,
   Button,
   Text,
   Link,
-  Center
+  Center,
+  Flex,
+  Switch,
 } from "@chakra-ui/core";
 
 import SnippetAPI from "../../api/snippet";
 
 const SettingsPopover = ({ snippetId, setLoadedSnippetId }) => {
-  function handleChange(event) {
-    const value = event.target.value;
+  function handleChange(value) {
+    if (!value) {
+      return;
+    }
     SnippetAPI.update(snippetId, {
       sid: value,
     }).then((snippet) => {
@@ -40,13 +47,52 @@ const SettingsPopover = ({ snippetId, setLoadedSnippetId }) => {
           <FiSettings size="1.2em" />
         </Button>
       </PopoverTrigger>
-      <PopoverContent>
+      <PopoverContent minWidth="25%">
         <PopoverArrow />
         <PopoverCloseButton />
         <PopoverHeader> Settings </PopoverHeader>
         <PopoverBody>
-          <FormLabel> Snippet ID </FormLabel>
-          <Input placeholder={snippetId} onChange={handleChange} />
+          <Flex
+            direction="column"
+            alignItems="stretch"
+            justifyContent="space-between"
+          >
+            <Flex
+              direction="row"
+              justifyContent="space-between"
+              alignItems="baseline"
+              marginBottom="1em"
+            >
+              <FormLabel> Snippet ID </FormLabel>
+              <Editable
+                placeholder={snippetId}
+                defaultValue={snippetId}
+                maxWidth="50%"
+                onSubmit={handleChange}
+              >
+                <EditablePreview />
+                <EditableInput />
+              </Editable>
+            </Flex>
+            <Flex
+              direction="row"
+              justifyContent="space-between"
+              alignItems="baseline"
+              marginBottom="1em"
+            >
+              <FormLabel> Read-Only </FormLabel>
+              <Switch maxWidth="50%"></Switch>
+            </Flex>
+            <Flex
+              direction="row"
+              justifyContent="space-between"
+              alignItems="baseline"
+              marginBottom="1em"
+            >
+              <FormLabel> Password </FormLabel>
+              <Input type="password" maxWidth="50%" />
+            </Flex>
+          </Flex>
         </PopoverBody>
         <PopoverFooter className="copyright">
           <Center>
@@ -56,6 +102,7 @@ const SettingsPopover = ({ snippetId, setLoadedSnippetId }) => {
               display="block"
               marginTop="8px"
               marginLeft="10px"
+              color="#3182CE"
             >
               <Text> &copy; Vladislav Andreev </Text>
             </Link>
